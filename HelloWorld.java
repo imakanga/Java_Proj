9714271cc0ebfc5a2f464144bf21dbef47f6d2c4
ivan makanga <ivanmakanga@gmail.com>
2022-09-23 17:01:58 +0300

    Java files

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    int x;
+    int y;
+    int z = x * y;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!! and The  Value for Z is: ");
+    }
+
+}
