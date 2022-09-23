1b9decb98f5e2618fb17adfa06ba7acfe86a475b
ivan makanga <ivanmakanga@gmail.com>
2022-09-23 17:26:34 +0300

    Updated java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
-public class HelloWorld {
-    int x;
-    int y;
-    int z = x * y;
+class HelloWorld {
+    static int x, y;
+    static int z = x * y;
 
     public static void main(String[] args) {
         System.out.println("Hello World !!! and The  Value for Z is: ");
+        System.out.println("The result is:" + z);
     }
 
 }
